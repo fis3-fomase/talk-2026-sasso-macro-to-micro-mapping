464c0251fa14fe449548076a3ad6b1da3b5ae8cf
Roberto Casadei <roberto.casadei90@gmail.com>
2026-08-28 10:28:39 +0200

    starting point

---
+++ slide-bottom.vue
@@ -0,0 +1,57 @@
+
+<script setup>
+
+const author = "R. Casadei, P. Baldini, N. Castronuovo"
+const logoUrl = '/imgs/unibo.png'
+</script>
+
+<template>
+    <div class="global-bottom" aria-hidden="true">
+        <div class="global-bottom__inner">
+            <div class="author"><span class="authors">{{ author }}</span> - University of Bologna</div>
+            <div class="counter">{{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}</div>
+        </div>
+        <div class="logo-wrap">
+            <!-- <img src="/imgs/unibo.png" alt="logo" class="logo" /> -->
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.global-bottom {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgb(187, 46, 41); /* red unibo */
+    color: white;
+    pointer-events: none;
+    z-index: 9999;
+    /*font-weight: bold;*/
+}
+.global-bottom__inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 8px 20px;
+    font-size: 0.75em;
+    opacity: 0.8;
+    /*font-weight: 500;*/
+}
+.logo-wrap {
+    margin-left: 12px;
+}
+.logo {
+    position: fixed;
+    right: -3em;
+    bottom: -2em;
+    width: 15em;
+    opacity: 0.2;
+    /* transform: rotate(-45deg); */
+    /* z-index: -1; */
+}
+.authors {
+    font-weight: 900;
+    font-family: sans serif;
+}
+</style>
---
+++ slides.md
@@ -0,0 +1,43 @@
+---
+# try also 'default' to start simple
+theme: default
+# random image from a curated Unsplash collection by Anthony
+# like them? see https://unsplash.com/collections/94734566/slidev
+background: /imgs/banner2026.png # https://cover.sli.dev
+# some information about your slides (markdown enabled)
+layout: cover
+title: "Macro-to-micro behavioural mappings in distributed systems: a characterisation on event structures"
+# info provides ..
+info: |
+  ## Slidev Starter Template
+  Presentation slides for developers.
+
+  Learn more at [Sli.dev](https://sli.dev)
+# apply UnoCSS classes to the current slide
+class: text-center
+# https://sli.dev/features/drawing
+drawings:
+  persist: false
+# slide transition: https://sli.dev/guide/animations.html#slide-transitions
+transition: slide-left
+# enable Comark Syntax: https://comark.dev/syntax/markdown
+comark: true
+# duration of the presentation
+duration: 5min
+
+---
+
+<style>
+#slidev-goto-dialog {
+  display: none !important;
+  visibility: hidden !important;
+  pointer-events: none !important;
+}
+</style>
+
+# Macro-to-micro behavioural mappings in distributed systems
+## A characterisation on event structures
+
+
+
+
